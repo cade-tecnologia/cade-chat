d26949f900c395db69e02161a91d0ee28e13cc24
UnDer-7 <mateus7532@gmail.com>
2019-05-11 17:22:51 -0300

    server addition

---
+++ server/src/socket/create-socket.main.ts
@@ -0,0 +1,30 @@
+import SocketIo from 'socket.io';
+import { Socket } from 'socket.io';
+import * as http from "http";
+import { MessageEvent } from './message.event';
+
+export class CreateSocket {
+  private io: SocketIo.Server;
+
+  constructor(io: http.Server) {
+    this.io = SocketIo.listen(io);
+    this.mainConnect();
+  }
+
+  /**
+   * Main connection.
+   * Every connection needs to be
+   * instantiated here.
+   */
+  private mainConnect(): void {
+    this.io.on('connect', (socket: Socket) => {
+      /**
+       * Trying to emmit an event to
+       * everybody connect.
+       * But it's not working with angular
+       */
+      socket.broadcast.emit('NOVO USUARIO CONECTADO!');
+      new MessageEvent(socket);
+    })
+  }
+}
